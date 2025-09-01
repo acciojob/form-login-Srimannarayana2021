557798e24df49e05efe59d4d20b80bc612855eec
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-09-01 14:29:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+event.preventDefault(); // stop form refresh
 
+      // Access form fields
+      const form = document.getElementById("form1");
+      const firstName = form.elements["fname"].value.trim();
+      const lastName = form.elements["lname"].value.trim();
+
+      // Concatenate and display
+      const fullName = firstName + " " + lastName;
+      alert(fullName);
 }
